46f067fbd92b2fb1d0f647a150cbd96b89efffe3
= <ricardo.tulio@fatec.sp.gov.br>
2016-06-23 08:46:49 -0300

    Implementando teste unitário para geração de fatura

--- src/main/java/br/com/ricardotulio/mikrotikadmin/controller/FaturasController.java
+++ src/main/java/br/com/ricardotulio/mikrotikadmin/controller/FaturasController.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -39,9 +40,8 @@ public class FaturasController {
 		return "faturas/index";
 	}
 
-	@Transactional
+	@Transactional(propagation = Propagation.NESTED)
 	public void geraProximasFaturas() {
-		System.out.println("Executando sistema de faturamento...");
 		Calendar dataFaturamentoTermino = Calendar.getInstance();
 
 		Calendar dataVencimento = Calendar.getInstance();
